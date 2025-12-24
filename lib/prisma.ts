e72e80a271a7267e4786c3ce53e946376463664a
Pranav Joshi <itspranavjoshi02@gmail.com>
2025-12-25 00:32:26 +0530

    fixes in product.ts and prisma.config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,17 @@
-import { PrismaClient } from "@/prisma/generated/client/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-});
-
 const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        adapter,
-    });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
--- types/product.ts
+++ types/product.ts
@@ -1,15 +1,15 @@
-import { Prisma } from "@/prisma/generated/client/client";
+import { Prisma } from "@prisma/client";
 import { Sale } from "./sale";
 
 export interface Product {
-    id: string;
-    name: string;
-    category: string;
-    stockQuantity: number;
-    stockThreshold: number;
-    costPrice: Prisma.Decimal;
-    sellingPrice: Prisma.Decimal;
-    sales: Sale[];
-    createdAt: Date;
-    updatedAt: Date;
+  id: string;
+  name: string;
+  category: string;
+  stockQuantity: number;
+  stockThreshold: number;
+  costPrice: Prisma.Decimal;
+  sellingPrice: Prisma.Decimal;
+  sales?: Sale[];
+  createdAt: Date;
+  updatedAt: Date;
 }
